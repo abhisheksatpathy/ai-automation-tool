fc3a7cb6ff01128ed33520e28d777258b99f6512
Abhishek Satpathy <abhisheksatpathy@Abhisheks-MacBook-Air.local>
2024-12-02 17:48:08 +0530

    test to check CORS error

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://abhisheksatpathy.github.io"],
+    allow_origins=["*"],  #"http://localhost:3000", "https://abhisheksatpathy.github.io"
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
