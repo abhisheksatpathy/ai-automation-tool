4a266118ff8637d43d759c91c2ffa2adaf64bd8c
Abhishek Satpathy <abhisheksatpathy@Abhisheks-MacBook-Air.local>
2024-12-02 22:29:59 +0530

    trial2

--- backend/tasks.py
+++ backend/tasks.py
@@ -1,6 +1,9 @@
 from celery_app import celery_app
 from openai import OpenAI
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 client = OpenAI(
     api_key=os.environ.get('OPENAI_API_KEY'),
